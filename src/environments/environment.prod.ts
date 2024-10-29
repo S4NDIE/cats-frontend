c0bf0dd02abb13070c668183e84b472d7d1afe75
santiago.hernandez <santiago.hernandez@xpertgroup.co>
2024-10-29 07:42:17 -0500

    Enviroment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: '__APIBACKENDCATS__',
+  tokenName: 'TW9tZW5yeS1CdXNpbmVzcy1Ub2tlbg==',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8084/',
+  tokenName: 'TW9tZW5yeS1CdXNpbmVzcy1Ub2tlbg==',
+};
